81ede6653deab40224f172b66dba52ec8bd9575d
Anthony Reis Soares <anthonyreis333@gmail.com>
2026-03-18 11:08:29 -0300

    feat: primeiro script Python + venv ativado - Dia 3 (20/03)

---
+++ hello_anthony.py
@@ -0,0 +1,4 @@
+print("Olá, mundo! Eu sou Anthony Reis Soares, tenho 21 anos de idade.")
+print("Dia 3 - 20/03/2026")
+print("Seguindo roadmap.sh com foco em Steve Jobs: simplicidade primeiro.")
+print("Ambiente venv ativado e funcionando!")
